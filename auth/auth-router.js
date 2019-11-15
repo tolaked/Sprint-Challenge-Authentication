512470bffb2c9239a53e0d432846dc962fcbd0ae
Akere Adetola <adetolaakere1@gmail.com>
2019-11-15 09:22:40 +0100

    register and login route

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,61 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
+const Users = require("../User/user-Model");
+
+router.post("/register", (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 11);
+  const newUser = {
+    username: req.body.username,
+    password: hash
+  };
+
+  Users.add(newUser)
+    .then(saved => {
+      const token = generateToken(saved);
+      res.status(201).json({ data: { user: saved, token } });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      // check if the provided password is correct
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res
+          .status(200)
+          .json({ message: `Welcome ${user.username}!`, token: token });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
+
+function generateToken(user) {
+  const result = jwt.sign(
+    { subject: user.department, username: user.username },
+    "D4RE!EFGF",
+    {
+      expiresIn: "96h"
+    }
+  );
+
+  return result;
+}
 
 module.exports = router;
